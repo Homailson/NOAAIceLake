c877ef3ec3e8d39d5bccfdbd91ebd06e42ce69b6
Homailson <homailson@gmail.com>
2025-06-12 07:35:39 -0300

    get statios ids done

--- lambdas/ingestion/handler.py
+++ lambdas/ingestion/handler.py
@@ -1,25 +1,31 @@
 from .modules.ingestion_utils.noaa_requests import get_all_results
 from .modules.ingestion_utils.save_to_s3 import save_stations_results
 import os
+import json
 
 def handler(event, context):
     print("Initializing data ingestion...")
 
-    datatypes = ['TMAX'] #'TMAX', 'TMIN', 'TAVG', 'PRCP'    
     URL_BASE = 'https://www.ncei.noaa.gov/cdo-web/api/v2/'
+
+    #Lambda environment variables
     API_KEY = os.environ.get("nooa_api_key")
-    startdate = event['start']
-    enddate = event['end']
+    datatypes = json.loads(os.environ.get("datatypes"))  #'TMAX', 'TMIN', 'TAVG', 'PRCP'
+    
+    period = {
+        'start': event['start'],
+        'end': event['end']
+    }
+    
+    print(f"Data ingestion started for {period['start']} to {period['end']}.")
 
-    print(f"Data ingestion started for {startdate} to {enddate}.")
+    stations_results = get_all_results(URL_BASE, API_KEY, datatypes, period['start'], period['end'])
 
-    stations_results = get_all_results(URL_BASE, API_KEY, datatypes, startdate, enddate)
     if not stations_results:
-        print(f"No data for {startdate}. Continuing to next period...")
+        print(f"No data for {period['start']}. Continuing to next period...")
     save_stations_results(stations_results)
 
-
     return {
-        "statusCode": 200,
-        "body": f"Data ingestion finished."
+        'datatypes': datatypes,
+        'period': period
     }
